e5bcef997b76ce9768717cf9db676d8fd66ea754
Андрей Клюев <undframe@gmail.com>
2020-11-19 21:01:49 +0400

    homework #1

--- src/main/java/ru/undframe/istucourse/Application.java
+++ src/main/java/ru/undframe/istucourse/Application.java
@@ -1,9 +1,22 @@
 package ru.undframe.istucourse;
 
+import ru.undframe.istucourse.model.Kotik;
+
+import java.util.Random;
+
 public class Application {
 
     public static void main(String[] args) {
 
+        Random random = new Random();
+
+        Kotik tom = new Kotik(random.nextInt(10) + 5, random.nextInt(20) + 5, "Том", "мяуууууууууууууу");
+        Kotik basya = new Kotik();
+        basya.setKotik(random.nextInt(10) + 5, random.nextInt(20) + 5, "Бася", "мяяяяяяяяяяяяу");
+
+        tom.liveAnotherDay();
+        System.out.println("Name: "+tom.getName()+" Возраст: "+tom.getWeight());
+        System.out.println(tom.getMeow().equals(basya.getMeow()));
     }
 
 }
---
+++ src/main/java/ru/undframe/istucourse/model/Kotik.java
@@ -0,0 +1,154 @@
+package ru.undframe.istucourse.model;
+
+import java.util.Objects;
+import java.util.Random;
+
+public class Kotik {
+
+    private static int kotikCount = 0;
+
+    private int prettiness;
+    private int weight;
+    private String name;
+    private String meow;
+    private int foodLvl;
+
+
+    public Kotik() {
+        kotikCount++;
+    }
+
+    public Kotik(int prettiness, int weight, String name, String meow) {
+        setKotik(prettiness, weight, name, meow);
+        kotikCount++;
+    }
+
+
+    public void setKotik(int prettiness, int weight, String name, String meow) {
+        this.prettiness = prettiness;
+        this.weight = weight;
+        this.name = name;
+        this.meow = meow;
+    }
+
+    public static int getKotikCount(){
+        return kotikCount;
+    }
+
+    public int getPrettiness() {
+        return prettiness;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMeow() {
+        return meow;
+    }
+
+    public boolean play(){
+        if(isHungry()){
+            askFood();
+            return false;
+        }
+
+        System.out.println("Playing... ");
+        foodLvl-=10;
+        return true;
+    }
+
+    public boolean sleep(){
+        if(isHungry()){
+            askFood();
+            return false;
+        }
+
+        System.out.println("Сплю...");
+        foodLvl-=1;
+        return true;
+    }
+
+    public boolean chaseMouse(){
+        if(isHungry()){
+            askFood();
+            return false;
+        }
+
+        System.out.println("поймал Джерри");
+        foodLvl-=3;
+        eat(30,"Джерри");
+        return true;
+    }
+
+    public void eat(int foodLvl) {
+        this.foodLvl+=foodLvl;
+        System.out.println("=поел("+foodLvl+")=");
+    }
+    public void eat(int foodLvl,String foodName) {
+        this.foodLvl+=foodLvl;
+        System.out.println("=поел("+foodLvl+"/"+foodName+")=");
+    }
+
+    public void eat(){
+        this.eat(10,"Whiskas");
+    }
+
+    public void liveAnotherDay(){
+        for (int i = 0; i < 24; i++) {
+            Random random = new Random();
+
+            int command = random.nextInt(3);
+
+            switch (command){
+                case 0:
+                    if(!play())
+                        eat();
+                case 1:
+                    if(!sleep())
+                        eat();
+                case 2:
+                    if(!chaseMouse())
+                        eat();
+            }
+
+
+        }
+    }
+
+    private boolean isHungry(){
+        return foodLvl<=0;
+    }
+
+    private void askFood(){
+        System.out.println("==Я ХОЧУ КУШАТЬ!!!!==");
+    }
+
+    @Override
+    public String toString() {
+        return "Kotik{" +
+                "prettiness=" + prettiness +
+                ", weight=" + weight +
+                ", name='" + name + '\'' +
+                ", meow='" + meow + '\'' +
+                ", foodLvl=" + foodLvl +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Kotik kotik = (Kotik) o;
+        return Objects.equals(meow, kotik.meow);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(meow);
+    }
+}
